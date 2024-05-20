e1daad31d5759eb02dbcec44230d696e9bdbb60e
xenia <janzen.xenia@gmx.de>
2024-05-20 15:34:27 +0200

    Bearbeitung Aufgabe 1.

--- src/main/java/aufgaben/eins/Kaffeautomat.java
+++ src/main/java/aufgaben/eins/Kaffeautomat.java
@@ -1,2 +1,12 @@
-package main.java.aufgaben.eins;public class Kaffeautomat {
+package main.java.aufgaben.eins;
+
+public class Kaffeautomat {
+	String bezeichnung;
+	String kaffeesorte;
+	Integer fuellstand;
+
+	void getBeschreibung() {
+		System.out.println("Kaffeautomat " + bezeichnung + " für " + kaffeesorte
+				+ " mit Füllstand " + fuellstand + "%");
+	}
 }
--- src/main/java/aufgaben/eins/Kantine.java
+++ src/main/java/aufgaben/eins/Kantine.java
@@ -1,2 +1,23 @@
-package main.java.aufgaben.eins;public class Kantine {
+package main.java.aufgaben.eins;
+
+public class Kantine {
+	public static void main(String[] args) {
+		Kaffeautomat kaffeeautomat = new Kaffeautomat();
+		kaffeeautomat.bezeichnung = "KVA-0815";
+		kaffeeautomat.kaffeesorte = "Kaffee";
+		kaffeeautomat.fuellstand = 100;
+
+		kaffeeautomat.getBeschreibung();
+
+		Kaffeautomat andererAutomat = new Kaffeautomat();
+		andererAutomat.bezeichnung = "KVA-007";
+		andererAutomat.kaffeesorte = "Cappuccino";
+		andererAutomat.fuellstand = 40;
+
+		andererAutomat.getBeschreibung();
+
+		andererAutomat.kaffeesorte = "Espresso";
+		andererAutomat.getBeschreibung();
+
+	}
 }
