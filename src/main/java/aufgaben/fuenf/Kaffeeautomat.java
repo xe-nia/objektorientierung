75f4a6058b0132ab071aecdd03dd4d903982b5c6
xenia <janzen.xenia@gmx.de>
2024-05-20 18:26:49 +0200

    Bearbeitung Aufgabe 5

---
+++ src/main/java/aufgaben/fuenf/Kaffeeautomat.java
@@ -0,0 +1,65 @@
+package main.java.aufgaben.fuenf;
+
+public class Kaffeeautomat {
+	private String bezeichnung;
+	private Kaffeesorte kaffeesorte;
+	private Integer fuellstand;
+
+	public Kaffeeautomat(String bezeichnung)
+	{
+		this(bezeichnung, Kaffeesorte.KAFFEE, 0);
+	}
+
+	public Kaffeeautomat(String bezeichnung, Kaffeesorte kaffeesorte, Integer fuellstand)
+	{
+		setBezeichnung(bezeichnung);
+		setKaffeesorte(kaffeesorte);
+		setFuellstand(fuellstand);
+	}
+	public String getBezeichnung() {
+		return bezeichnung;
+	}
+
+	public Kaffeesorte getKaffeesorte() {
+		return kaffeesorte;
+	}
+
+	public Integer getFuellstand() {
+		return fuellstand;
+	}
+
+	private void setBezeichnung(String neueBezeichnung) {
+		if (neueBezeichnung.equals("") || neueBezeichnung == null)
+		{
+			throw new IllegalArgumentException("Bezeichnung darf nicht leer sein.");
+		}
+		bezeichnung = neueBezeichnung;
+	}
+	public void setKaffeesorte(Kaffeesorte neueKaffeesorte) {
+		kaffeesorte = neueKaffeesorte;
+	}
+
+	private void setFuellstand(Integer neuerFuellstand) {
+		if (neuerFuellstand < 0 || neuerFuellstand > 100)
+		{
+			throw new IllegalArgumentException(neuerFuellstand + " kein gültiger Füllstand.");
+		}
+		fuellstand = neuerFuellstand;
+	}
+
+	String fuegeBohnenEin(Integer mengeInProzent)
+	{
+		Integer fuellmenge = mengeInProzent;
+		if (this.fuellstand + fuellmenge > 100)
+		{
+			fuellmenge = 100 - fuellstand;
+		}
+		setFuellstand(fuellstand + fuellmenge);
+		return(bezeichnung + ": " + fuellmenge + "% Bohnen aufgefüllt");
+	}
+	String getBeschreibung() {
+		return "Kaffeeautomat " + bezeichnung + " für " + kaffeesorte.getName()
+				+ " mit Füllstand " + fuellstand + "%";
+	}
+}
+
---
+++ src/main/java/aufgaben/fuenf/Kantine.java
@@ -0,0 +1,19 @@
+package main.java.aufgaben.fuenf;
+
+public class Kantine {
+	public static void main(String[] args) {
+		Kaffeeautomat kva0815 =
+				new Kaffeeautomat("KVA-0815", Kaffeesorte.KAFFEE, 100);
+		System.out.println(kva0815.getBeschreibung());
+
+		Kaffeeautomat kva007 =
+				new Kaffeeautomat("KVA-007", Kaffeesorte.CAPPUCCINO, 40);
+		System.out.println(kva007.getBeschreibung());
+		System.out.println(kva007.fuegeBohnenEin(70));
+		System.out.println(kva007.getBeschreibung());
+
+		kva007.setKaffeesorte(Kaffeesorte.ESPRESSO);
+		System.out.println(kva007.getBeschreibung());
+
+	}
+}
